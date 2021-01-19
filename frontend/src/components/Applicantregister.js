f7927410b24fa9f45bc9ee07f88aaeedac9bd0bb
pixel-z <zishankazi01@gmail.com>
2021-01-19 13:55:26 +0530

    Education in profile done

--- frontend/src/components/Applicantregister.js
+++ frontend/src/components/Applicantregister.js
@@ -15,7 +15,7 @@ export default class Applicantregister extends Component {
             password: '',
             rating: 0,
             skill: [],
-            education: [{institute:"", startyear:"", endyear:""}],
+            education: [{institute:'', startyear:'', endyear:''}],
             usertype: 'applicant'
         }
         this.onSubmit = this.onSubmit.bind(this);
@@ -46,35 +46,57 @@ export default class Applicantregister extends Component {
         })
     }
 
-    saveEducation = i => e => {
+    // saveEducation = i => e => {
+    //     const { name, value } = e.target;
+    //     const education = [...this.state.education];
+    //     education[i][name] = value;
+    //     this.setState({ education });
+    // }
+    // deleteEducation = i => e => {
+    //     e.preventDefault()
+    //     let education = [
+    //         ...this.state.education.slice(0, i),
+    //         ...this.state.education.slice(i + 1)
+    //     ]
+    //     this.setState({
+    //         education
+    //     })
+    // }
+    // addEducation = e => {
+    //     e.preventDefault()
+    //     let education = this.state.education.concat([{institute: '',startyear: '',endyear:''}])
+    //     this.setState({
+    //         education
+    //     })
+    // }
+    saveEducation = (index, e) => {
+        const { name, value } = e.target;
+        const education = [...this.state.education];
+        education[index][name] = value;
+        this.setState({ education });
+    }
+    addEducation = () => {
+        const item = {
+            institute: '',
+            startyear: '',
+            endyear: ''
+        }
+        this.setState({
+            education: [...this.state.education, item]
+        });
+    }
+    deleteEducation = (id) => {
         let education = [...this.state.education]
-        education[i]=e.target.value
+        education.splice(id, 1);
         this.setState({
             education
-        })
-    }
-    deleteEducation = i => e => {
-        e.preventDefault()
-        let education = [
-            ...this.state.education.slice(0, i),
-            ...this.state.education.slice(i + 1)
-        ]
-        this.setState({
-            education
-        })
-    }
-    addEducation = e => {
-        e.preventDefault()
-        let education = this.state.education.concat([{institute: '',startyear: '',endyear:''}])
-        this.setState({
-            education
-        })
+        });
     }
     
     onSubmit(e) {
         e.preventDefault();
-
-        // console.log("skill::::" + this.state.skill);
+        console.log("OBJECT:::::" + Object.keys(this.state.education));
+        console.log("education::::" + this.state.education);
 
         const newUser = {
             name: this.state.name,
@@ -83,9 +105,9 @@ export default class Applicantregister extends Component {
             rating: this.state.rating,
             usertype: this.state.usertype,
             skill: this.state.skill,
-            education: this.state.education,
+            education: this.state.education
         }
-        // console.log("newUser skill::::" + newUser.skill);
+        console.log("newUser skill::::" + newUser.skill);
 
         axios.post('http://localhost:4000/register', newUser)
             .then(res => {
@@ -107,12 +129,15 @@ export default class Applicantregister extends Component {
             password: '',
             rating: 0,
             skill: [],
-            education: [{institute:"", startyear:"", endyear:""}],
-            usertype: 'applicant',
+            education: [{institute:'', startyear:'', endyear:''}],
+            usertype: 'applicant'
         });
     }
 
     render() {
+        for (var i = 0; i < this.state.education.length; i++) {
+            console.log(this.state.education[i]);
+        }
         return (
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -159,25 +184,51 @@ export default class Applicantregister extends Component {
                         {this.state.skill.map((skill, index) => (
                         <span key={index}>
                             <input type="text" onChange={this.saveSkill(index)} value={skill} />
-                            <button onClick={this.deleteSkill(index)}>X</button>
+                            <button onClick={this.deleteSkill(index)}>x</button>
                         </span>
                         ))}
                         <br/>
                         <button onClick={this.addSkill}>Add Skill</button>
                     </div>
 
-                    <div className="form-group">
+                    {/* <div className="form-group">
                         <label>Education: </label>
                         <br/>
-                        {this.state.education.map((education, index) => (
-                        <span key={index}>
-                            <input type="text" onChange={this.saveEducation(index)} value={education} />
-                            <button onClick={this.deleteEducation(index)}>X</button>
-                        </span>
-                        ))}
-                        <br/>
-                        <button onClick={this.addeducation}>Add Education</button>
-                    </div>
+                        {
+                            this.state.education.map((education, index) => {
+                                return (
+                                    <div className="box">
+                                        <input type="text" placeholder="Institute" onChange={this.saveEducation.bind(this,index)} value={education.institute} />
+                                        <input type="text" placeholder="startYear" onChange={this.saveEducation.bind(this,index)} value={education.startyear} />
+                                        <input type="text" placeholder="endYear" onChange={this.saveEducation.bind(this,index)} value={education.endyear} />
+                                        <button onClick={this.deleteEducation(index)}>x</button>
+                                        <button onClick={this.addEducation}>+</button>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div> */}
+                    <label>Education:</label>
+                        <div>
+                            {
+                                this.state.education.map((x, i) => {
+                                    return (
+                                        <div className="box">
+                                            <input name="institute" placeholder="institute" value={x.institute} onChange={this.saveEducation.bind(this, i)} />
+                                            <input name="startyear" placeholder="startyear" value={x.startyear} onChange={this.saveEducation.bind(this, i)} />
+                                            <input name="endyear" placeholder="endyear" value={x.endyear} onChange={this.saveEducation.bind(this, i)} />
+                                            <div className="btn-box">
+                                                {this.state.education.length !== 1 && <input type='button' value='remove' onClick={this.deleteEducation.bind(this, i)} />}
+
+                                                {this.state.education.length - 1 === i && <input type='button' value='add more' onClick={this.addEducation.bind(this)} style={{marginLeft:8}}/>}
+                                            </div>
+
+                                        </div>
+                                    );
+                                })
+                            }
+
+                        </div>
                     
                     <div className="form-group">
                         <label>Rating: </label>
